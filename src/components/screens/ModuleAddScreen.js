bcff458e09f38d426d818e15d96d34d2a1c5fa50
naseroish <82692921+naseroish@users.noreply.github.com>
2024-10-10 08:13:23 +0100

    #

--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,15 +1,33 @@
 
 import { StyleSheet, Text } from 'react-native';
 import Screen from '../layout/Screen';
+import { ButtonTray, Button } from "../UI/Button.js";
+import Icons from "../UI/Icons.js";
 
-const ModuleAddScreen = () => {
+const defaultModule = {
+  ModuleCode: Math.floor( 100000 + Math.random() * 900000 ),
+  ModuleCode: 'CI6330',
+  ModuleName: 'Mobile Application Development',
+  ModuleLevel: 6,
+  ModuleLeaderID: 1,
+  ModuleImage: 'https://images.freeimages.com/images/small-previews/cf5/cellphone-1313194.jpg',
+};
+
+const ModuleAddScreen = ({ navigation, route }) => {
   // Initialisations ---------------------------------
+  const { onAdd } = route.params;
   // State -------------------------------------------
   // Handlers ----------------------------------------
+  const handleAdd = () => onAdd(defaultModule);
+  const handleCancel = navigation.goBack;
   // View --------------------------------------------
   return (
     <Screen>
       <Text>Add</Text>
+      <ButtonTray>
+        <Button label="Save" icon={<Icons.Add />} onClick={handleAdd} />
+        <Button label="Cancel" onClick={handleCancel} />
+      </ButtonTray>
     </Screen>
   );
 }
