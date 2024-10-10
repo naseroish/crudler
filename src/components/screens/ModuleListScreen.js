8b797425aa2839ec043430a3eef21dac3c2d994c
Naser <82692921+naseroish@users.noreply.github.com>
2024-10-10 08:14:22 +0100

    # (#9)

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -3,6 +3,8 @@ import { LogBox, StyleSheet } from "react-native";
 import Screen from "../layout/Screen";
 import intialModules from "../../data/modules.js";
 import ModuleList from "../entity/modules/ModuleList.js";
+import { ButtonTray, Button } from "../UI/Button.js";
+import Icons from "../UI/Icons.js";
 
 const ModuleListScreen = ({ navigation }) => {
   // Initialisations ---------------------------------
@@ -13,16 +15,27 @@ const ModuleListScreen = ({ navigation }) => {
   const handleDelete = (module) => 
     setModules(modules.filter((m) => m.ModuleID !== module.ModuleID));
 
+  const handleAdd = (module) => setModules([...modules, module]);
+
   const onDelete = (module) => {
     handleDelete(module);
     navigation.goBack();
   };
 
-  const handleSelect = (module) => navigation.navigate('ModuleViewScreen', { module, onDelete });
+  const onAdd = (module) => {
+    handleAdd(module);
+    navigation.goBack();
+  };
+
+  const goToViewScreen = (module) => navigation.navigate('ModuleViewScreen', { module, onDelete });
+  const goToAddScreen = () => navigation.navigate('ModuleAddScreen', { onAdd });
   // View --------------------------------------------
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect} />
+      <ButtonTray>
+        <Button label="Add" icon={<Icons.Add/>} onClick={goToAddScreen} />
+      </ButtonTray>
+      <ModuleList modules={modules} onSelect={goToViewScreen} />
     </Screen>
   );
 };
