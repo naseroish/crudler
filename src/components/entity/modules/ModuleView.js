a882cb3ae5903dbced51872cbefddb7458485a11
naseroish <82692921+naseroish@users.noreply.github.com>
2024-10-14 17:58:43 +0100

    fetching modules from api and implmenting loading

--- src/components/entity/modules/ModuleView.js
+++ src/components/entity/modules/ModuleView.js
@@ -21,7 +21,7 @@ const ModuleView = ({ module, onDelete, onModify }) => {
   return (
     <View style={styles.container}>
       <FullWidthImage
-        source={{ uri: module.ModuleImage }}
+        source={{ uri: module.ModuleImageURL }}
         style={styles.image}
       />
 
